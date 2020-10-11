3041a0d5750047e3627ee0e58f8ad3a03ab81ed6
Mustafa Enes Güneruz <56077952+menesguneruz@users.noreply.github.com>
2020-10-11 13:49:04 +0300

    Add files via upload

---
+++ numbers.rb
@@ -0,0 +1,12 @@
+# 2 ** 3 = 2^3
+num = 7
+puts ("My favourite num is " +num.to_s)
+puts num.abs() # sayının pozitif değerini (sayı pozitifse kendisini) verir.
+
+num2 = 5.356
+puts num2.round() #sayının tam sayıya yuvarlar.
+puts num2.ceil() # sayıyı bir üst tam sayıya yuvarlar.
+puts num2.floor() #sayıyı aşağısındaki tam sayıya yuvarlar. (tam sayı kısmına)
+
+puts Math.sqrt(49) # Math kütüphanesinin bir fonksiyonu. Sayının kökünü verir.
+puts Math.log(3) # Logaritma fonksiyonu.
