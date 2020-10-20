5b9d7cebd0b59a309f3930b63449828e9158a06b
Mustafa Enes Güneruz <56077952+menesguneruz@users.noreply.github.com>
2020-10-20 16:25:34 +0300

    Add files via upload

---
+++ HelloWorld.rb
@@ -0,0 +1,18 @@
+puts “hello“
+ 
+ puts “34” + “55”
+ 
+ puts “world”
+ 
+
+ 
+ #comment lines
+ #syntax highlighting
+ #file extension .rb
+ 
+ #dosya adı . dosya uzantısı
+ #hata.rb
+ #white space(beyaz boşluk): space, tab, newline
+ #türkçe karakterler: ğĞüÜöÖçÇşŞıİ
+ #daima küçük harf kullan
+ #filanca_kisinin_dilekcesi.rb
