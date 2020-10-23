ab30715804b27f315156692dcb012c4e2373c7b2
Mustafa Enes Güneruz <56077952+menesguneruz@users.noreply.github.com>
2020-10-23 14:11:43 +0300

    Update input.rb

--- input.rb
+++ input.rb
@@ -1,3 +1,10 @@
 print "Enter your name: "
 name = gets.chomp() # .chomp() fonksiyonu isimden sonraki \n i siler.
 puts ("Hello " + name + ", You're cool")
+
+print "Enter first number:"
+firstNumber = gets.to_i
+print "Enter second number:"
+secondNumber = gets.to_i
+
+puts firstNumber.to_s + " + " + secondNumber.to_s + " = " + (firstNumber + secondNumber).to_s
